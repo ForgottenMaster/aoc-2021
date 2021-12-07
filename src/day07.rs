dc270b75739b7f825d2eeea6dbaa2704a7f6ea10
Robin Firth <theforgottenmaster@googlemail.com>
2021-12-07 15:32:16 +0000

    Added solution for part 2 of day 7

--- src/day07.rs
+++ src/day07.rs
@@ -9,17 +9,21 @@ use std::{
 pub fn run() -> Result<(Box<dyn Display>, Box<dyn Display>), Box<dyn Error>> {
     let file = File::open("input/day07.txt")?;
     let reader = BufReader::new(file);
-    let crab_position_counts = read_crab_position_counts(reader);
-    let part_1 = calculate_minimal_fuel_cost_to_align(&crab_position_counts);
-    Ok((Box::new(part_1), Box::new(0)))
+    let (crab_position_counts, min, max) = read_crab_position_counts(reader);
+    let part_1 = calculate_minimal_fuel_cost_to_align(&crab_position_counts, min, max, 0);
+    let part_2 = calculate_minimal_fuel_cost_to_align(&crab_position_counts, min, max, 1);
+    Ok((Box::new(part_1), Box::new(part_2)))
 }
 
 /// Takes a given BufReader that should contain a sequence of comma separated
 /// unsigned integers and returns a HashMap containing the positions mapped to the
 /// number that are at that position (this is because we will always move all crabs at a given
 /// position rather than individual crabs).
-fn read_crab_position_counts(reader: impl BufRead) -> HashMap<u32, u32> {
+/// Along with the hashmap of positions returns the minimum and maximum position that has a crab on it.
+fn read_crab_position_counts(reader: impl BufRead) -> (HashMap<u32, u32>, u32, u32) {
     let mut hm = HashMap::new();
+    let mut min = u32::MAX;
+    let mut max = u32::MIN;
     reader
         .lines()
         .filter_map(|line| line.ok())
@@ -36,9 +40,11 @@ fn read_crab_position_counts(reader: impl BufRead) -> HashMap<u32, u32> {
                 })
                 .for_each(|elem: u32| {
                     *hm.entry(elem).or_insert(0) += 1;
+                    min = std::cmp::min(min, elem);
+                    max = std::cmp::max(max, elem);
                 });
         });
-    hm
+    (hm, min, max)
 }
 
 /// Takes a reference to a mapping of crab position counts along with a given position
@@ -46,6 +52,7 @@ fn read_crab_position_counts(reader: impl BufRead) -> HashMap<u32, u32> {
 fn calculate_fuel_required_for_position(
     position_counts: &HashMap<u32, u32>,
     target_position: u32,
+    fuel_acceleration: u32,
 ) -> u32 {
     position_counts
         .into_iter()
@@ -56,16 +63,33 @@ fn calculate_fuel_required_for_position(
                 (*position, target_position)
             };
             let diff = max - min;
-            diff * count // required as we're shifting all crabs at that position
+            calculate_fuel_cost_from_distance(diff, fuel_acceleration) * count // required as we're shifting all crabs at that position
+        })
+        .sum()
+}
+
+fn calculate_fuel_cost_from_distance(steps: u32, fuel_acceleration: u32) -> u32 {
+    (0..steps)
+        .scan(1, |cost, _| {
+            let price = *cost;
+            *cost += fuel_acceleration;
+            Some(price)
         })
         .sum()
 }
 
 /// Calculates the minimal fuel cost for arranging the crabs.
-fn calculate_minimal_fuel_cost_to_align(position_counts: &HashMap<u32, u32>) -> u32 {
-    position_counts
-        .iter()
-        .map(|(position, _)| calculate_fuel_required_for_position(position_counts, *position))
+fn calculate_minimal_fuel_cost_to_align(
+    position_counts: &HashMap<u32, u32>,
+    min_pos: u32,
+    max_pos: u32,
+    fuel_acceleration: u32,
+) -> u32 {
+    (min_pos..=max_pos)
+        .into_iter()
+        .map(|position| {
+            calculate_fuel_required_for_position(position_counts, position, fuel_acceleration)
+        })
         .min()
         .unwrap()
 }
@@ -83,28 +107,75 @@ mod tests {
         let expected = [(0, 1), (1, 2), (2, 3), (4, 1), (7, 1), (14, 1), (16, 1)]
             .into_iter()
             .collect::<HashMap<_, _>>();
-        let calculated = read_crab_position_counts(INPUT);
+        let (calculated, _, _) = read_crab_position_counts(INPUT);
         assert_eq!(expected, calculated);
     }
 
     #[test]
-    fn test_calculate_fuel_required_for_position() {
+    fn test_calculate_fuel_required_for_position_constant_acceleration() {
         let input_counts = [(0, 1), (1, 2), (2, 3), (4, 1), (7, 1), (14, 1), (16, 1)]
             .into_iter()
             .collect::<HashMap<_, _>>();
         let input_position = 2;
         let expected = 37;
-        let calculated = calculate_fuel_required_for_position(&input_counts, input_position);
+        let calculated = calculate_fuel_required_for_position(&input_counts, input_position, 0);
         assert_eq!(expected, calculated);
     }
 
     #[test]
-    fn test_calculate_minimal_fuel_cost_to_align() {
+    fn test_calculate_fuel_required_for_position_ramping_acceleration() {
+        let input_counts = [(0, 1), (1, 2), (2, 3), (4, 1), (7, 1), (14, 1), (16, 1)]
+            .into_iter()
+            .collect::<HashMap<_, _>>();
+        let input_position = 2;
+        let expected = 206;
+        let calculated = calculate_fuel_required_for_position(&input_counts, input_position, 1);
+        assert_eq!(expected, calculated);
+    }
+
+    #[test]
+    fn test_calculate_minimal_fuel_cost_to_align_constant_acceleration() {
         let input_counts = [(0, 1), (1, 2), (2, 3), (4, 1), (7, 1), (14, 1), (16, 1)]
             .into_iter()
             .collect::<HashMap<_, _>>();
         let expected = 37;
-        let calculated = calculate_minimal_fuel_cost_to_align(&input_counts);
+        let calculated = calculate_minimal_fuel_cost_to_align(&input_counts, 0, 16, 0);
+        assert_eq!(expected, calculated);
+    }
+
+    #[test]
+    fn test_calculate_minimal_fuel_cost_to_align_ramping_acceleration() {
+        let input_counts = [(0, 1), (1, 2), (2, 3), (4, 1), (7, 1), (14, 1), (16, 1)]
+            .into_iter()
+            .collect::<HashMap<_, _>>();
+        let expected = 168;
+        let calculated = calculate_minimal_fuel_cost_to_align(&input_counts, 0, 16, 1);
+        assert_eq!(expected, calculated);
+    }
+
+    #[test]
+    fn test_calculate_fuel_cost_from_distance_ramping() {
+        assert_eq!(calculate_fuel_cost_from_distance(3, 0), 3);
+        assert_eq!(calculate_fuel_cost_from_distance(3, 1), 6);
+        assert_eq!(calculate_fuel_cost_from_distance(10, 0), 10);
+        assert_eq!(calculate_fuel_cost_from_distance(10, 1), 55);
+        assert_eq!(calculate_fuel_cost_from_distance(11, 1), 66);
+        assert_eq!(calculate_fuel_cost_from_distance(4, 1), 10);
+        assert_eq!(calculate_fuel_cost_from_distance(5, 1), 15);
+        assert_eq!(calculate_fuel_cost_from_distance(1, 1), 1);
+        assert_eq!(calculate_fuel_cost_from_distance(2, 1), 3);
+        assert_eq!(calculate_fuel_cost_from_distance(9, 1), 45);
+        assert_eq!(calculate_fuel_cost_from_distance(0, 1), 0);
+    }
+
+    #[test]
+    fn test_calculate_fuel_required_for_position_4() {
+        let input_counts = [(0, 1), (1, 2), (2, 3), (4, 1), (7, 1), (14, 1), (16, 1)]
+            .into_iter()
+            .collect::<HashMap<_, _>>();
+        let input_position = 5;
+        let expected = 168;
+        let calculated = calculate_fuel_required_for_position(&input_counts, input_position, 1);
         assert_eq!(expected, calculated);
     }
 }
