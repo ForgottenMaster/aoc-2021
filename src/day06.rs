0b70f8a3b607ba639538daaa8d5cef627559958a
Robin Firth <theforgottenmaster@googlemail.com>
2021-12-07 10:18:35 +0000

    Fixed part 2 of day 6. Turns out it must have been overflowing/wrapping around the u32. Weird though as it should panic if it's performing non-wrapping addition

--- src/day06.rs
+++ src/day06.rs
@@ -15,13 +15,13 @@ pub fn run() -> Result<(Box<dyn Display>, Box<dyn Display>), Box<dyn Error>> {
     let reader = BufReader::new(file);
     let mut fish_counts = read_lanternfish_counts(reader)?;
     simulate_days(&mut fish_counts, 80);
-    let part_1: u32 = fish_counts.iter().map(|(_, value)| value).sum();
+    let part_1: u128 = fish_counts.iter().map(|(_, value)| value).sum();
     simulate_days(&mut fish_counts, 176); // part 2 is 176 iterations more than part 1
-    let part_2: u32 = fish_counts.iter().map(|(_, value)| value).sum();
+    let part_2: u128 = fish_counts.iter().map(|(_, value)| value).sum();
     Ok((Box::new(part_1), Box::new(part_2)))
 }
 
-fn read_lanternfish_counts(reader: impl BufRead) -> Result<HashMap<u8, u32>, ParseIntError> {
+fn read_lanternfish_counts(reader: impl BufRead) -> Result<HashMap<u8, u128>, ParseIntError> {
     let mut fish_counts = HashMap::new();
     for line in reader.lines().filter_map(|line| Some(line.ok()?)) {
         for elem in line.trim().split(",") {
@@ -31,7 +31,7 @@ fn read_lanternfish_counts(reader: impl BufRead) -> Result<HashMap<u8, u32>, Par
     Ok(fish_counts)
 }
 
-fn simulate_one_day(fish_counts: &mut HashMap<u8, u32>) {
+fn simulate_one_day(fish_counts: &mut HashMap<u8, u128>) {
     // cache the number of fish currently at 0 (that will be added on at the end, and will spawn new fish).
     let number_of_zeroes = {
         let entry = fish_counts.entry(0).or_insert(0);
@@ -53,7 +53,7 @@ fn simulate_one_day(fish_counts: &mut HashMap<u8, u32>) {
     *fish_counts.entry(SPAWN_AT).or_insert(0) += number_of_zeroes;
 }
 
-fn simulate_days(fish_counts: &mut HashMap<u8, u32>, days: u32) {
+fn simulate_days(fish_counts: &mut HashMap<u8, u128>, days: u128) {
     (0..days).for_each(|_| simulate_one_day(fish_counts));
 }
 
@@ -69,7 +69,7 @@ mod tests {
         let expected_counts = read_lanternfish_counts(EXPECTED).unwrap();
         simulate_days(&mut fish_counts, 1);
         assert_eq!(
-            fish_counts.iter().map(|(_, value)| value).sum::<u32>(),
+            fish_counts.iter().map(|(_, value)| value).sum::<u128>(),
             expected_counts.iter().map(|(_, value)| value).sum()
         );
     }
@@ -82,8 +82,26 @@ mod tests {
         let expected_counts = read_lanternfish_counts(EXPECTED).unwrap();
         simulate_days(&mut fish_counts, 5);
         assert_eq!(
-            fish_counts.iter().map(|(_, value)| value).sum::<u32>(),
+            fish_counts.iter().map(|(_, value)| value).sum::<u128>(),
             expected_counts.iter().map(|(_, value)| value).sum()
         );
+    }
+    
+    #[test]
+    fn test_example_part_1() {
+        const INPUT: &[u8] = "3,4,3,1,2".as_bytes();
+        const EXPECTED: u128 = 5934;
+        let mut fish_counts = read_lanternfish_counts(INPUT).unwrap();
+        simulate_days(&mut fish_counts, 80);
+        assert_eq!(fish_counts.iter().map(|(_, value)| value).sum::<u128>(), EXPECTED);
+    }
+    
+    #[test]
+    fn test_example_part_2() {
+        const INPUT: &[u8] = "3,4,3,1,2".as_bytes();
+        const EXPECTED: u128 = 26984457539;
+        let mut fish_counts = read_lanternfish_counts(INPUT).unwrap();
+        simulate_days(&mut fish_counts, 256);
+        assert_eq!(fish_counts.iter().map(|(_, value)| value).sum::<u128>(), EXPECTED);
     }
 }
