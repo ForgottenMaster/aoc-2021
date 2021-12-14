081b521fd98bec421b47cfe75ce6d16baa288c25
Robin Firth <theforgottenmaster@googlemail.com>
2021-12-14 01:57:27 +0000

    Added tests for code coverage to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,50 @@
 use {
     aoc_2021::*,
-    std::{env::args, error::Error, io, io::ErrorKind, time::Instant},
+    std::{env::args, time::Instant},
 };
 
-fn main() -> Result<(), Box<dyn Error>> {
-    if let Some(solution_number) = args().skip(1).next() {
+fn main() {
+    main_internal(args());
+}
+
+fn main_internal(args: impl Iterator<Item = String>) {
+    if let Some(solution_number) = args.skip(1).next() {
         let start_time = Instant::now();
-        run_with(solution_number.parse::<usize>()?, |part_1, part_2| {
-            let elapsed = start_time.elapsed().as_micros();
-            println!("Part 1 => {}", part_1);
-            println!("Part 2 => {}", part_2);
-            println!("Took {} microseconds", elapsed);
-        });
-        Ok(())
+        run_with(
+            solution_number
+                .parse::<usize>()
+                .expect("Couldn't parse the provided solution number as an integer."),
+            |part_1, part_2| {
+                let elapsed = start_time.elapsed().as_micros();
+                println!("Part 1 => {}", part_1);
+                println!("Part 2 => {}", part_2);
+                println!("Took {} microseconds", elapsed);
+            },
+        );
     } else {
-        Err(Box::new(io::Error::new(
-            ErrorKind::InvalidInput,
-            "Argument list to program requires an entry indicating the day number to run.",
-        )))
+        panic!("Argument list to program requires an entry indicating the day number to run.");
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // Test here just to allow code coverage to be reported as 100%
+    #[test]
+    #[should_panic]
+    fn test_main() {
+        main();
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_main_internal_invalid_argument() {
+        main_internal(vec!["program_path".to_string(), "foo".to_string()].into_iter());
+    }
+
+    #[test]
+    fn test_main_internal_valid_argument() {
+        main_internal(vec!["program_path".to_string(), "1".to_string()].into_iter());
     }
 }
