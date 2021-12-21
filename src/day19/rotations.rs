8e3fed533470559ffbde789cec20f0552f665070
Robin Firth <theforgottenmaster@googlemail.com>
2021-12-21 23:51:26 +0000

    Finalise the set of 24 unique rotations that we will use

--- src/day19/rotations.rs
+++ src/day19/rotations.rs
@@ -13,7 +13,7 @@ const COS_180_DEG: i64 = -1;
 const COS_270_DEG: i64 = 0;
 
 // Enum to identify angle of rotation around one axis
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug)]
 enum Angle {
     Zero,
     Ninety,
@@ -108,7 +108,7 @@ const fn mult_mat(mat_1: [[i64; 3]; 3], mat_2: [[i64; 3]; 3]) -> [[i64; 3]; 3] {
 }
 
 /// Matrix multiplication isn't commutative so this defines the order of rotation.
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 enum RotationOrder {
     XYZ,
     XZY,
@@ -119,69 +119,41 @@ enum RotationOrder {
 }
 
 /// Produces the matrix for a given sequence of rotations with a given
-/// angle set.
-const fn rotation_matrix(x: Angle, y: Angle, z: Angle, order: RotationOrder) -> Matrix<i64, 3, 3> {
-    Matrix::new(match order {
-        RotationOrder::XYZ => mult_mat(mult_mat(rotation_x(x), rotation_y(y)), rotation_z(z)),
-        RotationOrder::XZY => mult_mat(mult_mat(rotation_x(x), rotation_z(z)), rotation_y(y)),
-        RotationOrder::YXZ => mult_mat(mult_mat(rotation_y(y), rotation_x(x)), rotation_z(z)),
-        RotationOrder::YZX => mult_mat(mult_mat(rotation_y(y), rotation_z(z)), rotation_x(x)),
-        RotationOrder::ZXY => mult_mat(mult_mat(rotation_z(z), rotation_x(x)), rotation_y(y)),
-        RotationOrder::ZYX => mult_mat(mult_mat(rotation_z(z), rotation_y(y)), rotation_x(x)),
-    })
-}
-
-/// Produces all possible rotation matrices for a given RotationOrder (including duplicates as it's easier
-/// we can filter these out with a HashSet later at runtime).
-const fn all_possible_matrices(order: RotationOrder) -> [Matrix<i64, 3, 3>; 24] {
-    [
-        rotation_matrix(Angle::Zero, Angle::Ninety, Angle::OneEighty, order),
-        rotation_matrix(Angle::Zero, Angle::Ninety, Angle::TwoSeventy, order),
-        rotation_matrix(Angle::Zero, Angle::OneEighty, Angle::Ninety, order),
-        rotation_matrix(Angle::Zero, Angle::OneEighty, Angle::TwoSeventy, order),
-        rotation_matrix(Angle::Zero, Angle::TwoSeventy, Angle::Ninety, order),
-        rotation_matrix(Angle::Zero, Angle::TwoSeventy, Angle::OneEighty, order),
-        rotation_matrix(Angle::Ninety, Angle::Zero, Angle::OneEighty, order),
-        rotation_matrix(Angle::Ninety, Angle::Zero, Angle::TwoSeventy, order),
-        rotation_matrix(Angle::Ninety, Angle::OneEighty, Angle::Zero, order),
-        rotation_matrix(Angle::Ninety, Angle::OneEighty, Angle::TwoSeventy, order),
-        rotation_matrix(Angle::Ninety, Angle::TwoSeventy, Angle::Zero, order),
-        rotation_matrix(Angle::Ninety, Angle::TwoSeventy, Angle::OneEighty, order),
-        rotation_matrix(Angle::OneEighty, Angle::Zero, Angle::Ninety, order),
-        rotation_matrix(Angle::OneEighty, Angle::Zero, Angle::TwoSeventy, order),
-        rotation_matrix(Angle::OneEighty, Angle::Ninety, Angle::Zero, order),
-        rotation_matrix(Angle::OneEighty, Angle::Ninety, Angle::TwoSeventy, order),
-        rotation_matrix(Angle::OneEighty, Angle::TwoSeventy, Angle::Zero, order),
-        rotation_matrix(Angle::OneEighty, Angle::TwoSeventy, Angle::Ninety, order),
-        rotation_matrix(Angle::TwoSeventy, Angle::Zero, Angle::Ninety, order),
-        rotation_matrix(Angle::TwoSeventy, Angle::Zero, Angle::OneEighty, order),
-        rotation_matrix(Angle::TwoSeventy, Angle::Ninety, Angle::Zero, order),
-        rotation_matrix(Angle::TwoSeventy, Angle::Ninety, Angle::OneEighty, order),
-        rotation_matrix(Angle::TwoSeventy, Angle::OneEighty, Angle::Zero, order),
-        rotation_matrix(Angle::TwoSeventy, Angle::OneEighty, Angle::Ninety, order),
-    ]
+/// angle set. Order of rotation is Z, Y, then X.
+const fn rotation_matrix(x: Angle, y: Angle, z: Angle) -> Matrix<i64, 3, 3> {
+    Matrix::new(mult_mat(
+        mult_mat(rotation_x(x), rotation_y(y)),
+        rotation_z(z),
+    ))
 }
 
 // all the possible rotation matrices with all possible orders of multiplication.
-const ROTATIONS: [[Matrix<i64, 3, 3>; 24]; 6] = [
-    all_possible_matrices(RotationOrder::XYZ),
-    all_possible_matrices(RotationOrder::XZY),
-    all_possible_matrices(RotationOrder::YXZ),
-    all_possible_matrices(RotationOrder::YZX),
-    all_possible_matrices(RotationOrder::ZXY),
-    all_possible_matrices(RotationOrder::ZYX),
+const ROTATIONS: [Matrix<i64, 3, 3>; 24] = [
+    rotation_matrix(Angle::Zero, Angle::Zero, Angle::Ninety),
+    rotation_matrix(Angle::Ninety, Angle::OneEighty, Angle::OneEighty),
+    rotation_matrix(Angle::Ninety, Angle::OneEighty, Angle::Zero),
+    rotation_matrix(Angle::Zero, Angle::Zero, Angle::TwoSeventy),
+    rotation_matrix(Angle::Zero, Angle::Ninety, Angle::TwoSeventy),
+    rotation_matrix(Angle::Zero, Angle::Zero, Angle::Zero),
+    rotation_matrix(Angle::Zero, Angle::TwoSeventy, Angle::Zero),
+    rotation_matrix(Angle::Zero, Angle::TwoSeventy, Angle::OneEighty),
+    rotation_matrix(Angle::Zero, Angle::Zero, Angle::OneEighty),
+    rotation_matrix(Angle::Zero, Angle::OneEighty, Angle::TwoSeventy),
+    rotation_matrix(Angle::Ninety, Angle::OneEighty, Angle::TwoSeventy),
+    rotation_matrix(Angle::Zero, Angle::OneEighty, Angle::Ninety),
+    rotation_matrix(Angle::Zero, Angle::OneEighty, Angle::OneEighty),
+    rotation_matrix(Angle::Zero, Angle::Ninety, Angle::OneEighty),
+    rotation_matrix(Angle::Ninety, Angle::Zero, Angle::TwoSeventy),
+    rotation_matrix(Angle::Zero, Angle::Ninety, Angle::Ninety),
+    rotation_matrix(Angle::Zero, Angle::Ninety, Angle::Zero),
+    rotation_matrix(Angle::Ninety, Angle::Zero, Angle::Ninety),
+    rotation_matrix(Angle::Ninety, Angle::Zero, Angle::OneEighty),
+    rotation_matrix(Angle::Zero, Angle::TwoSeventy, Angle::Ninety),
+    rotation_matrix(Angle::Ninety, Angle::OneEighty, Angle::Ninety),
+    rotation_matrix(Angle::Zero, Angle::OneEighty, Angle::Zero),
+    rotation_matrix(Angle::Ninety, Angle::Zero, Angle::Zero),
+    rotation_matrix(Angle::Zero, Angle::TwoSeventy, Angle::TwoSeventy),
 ];
-
-// function that produces all the **unique** rotation matrices as a HashSet. This
-// isn't all possible ones as different orders of rotation can result in the same
-// final orientation. However it was easier to calculate all possible rotations in
-// all possible orderings and then just filter out duplicates.
-pub fn all_unique_rotations() -> HashSet<Matrix<i64, 3, 3>> {
-    ROTATIONS
-        .into_iter()
-        .flat_map(|arr| arr.into_iter())
-        .collect()
-}
 
 #[cfg(test)]
 mod tests {
@@ -189,7 +161,6 @@ mod tests {
 
     #[test]
     fn test_rotation_count() {
-        dbg!(all_unique_rotations().len());
-        unimplemented!()
+        assert_eq!(ROTATIONS.into_iter().collect::<HashSet<_>>().len(), 24);
     }
 }
